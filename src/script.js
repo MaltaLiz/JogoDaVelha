f091b4f2ab4af85e08598ce6f75fee942126a9a6
F6293829 Lizandra Regis Malta <lizandra.malta@bb.com.br>
2022-06-24 11:36:09 -0300

    Adicionado a funcionalidade do ganhador ter o direito de comecar

--- src/script.js
+++ src/script.js
@@ -16,24 +16,32 @@ const casosDeSucesso =
         [1, 5, 9],
         [3, 5, 7]
     ];
+const vez = {
+    'jogador1': true,
+    'jogador2': false
+}
+
+sessionStorage.setItem('vez', JSON.stringify(vez));
 
 jogador1.textContent = verificaPlacar('jogador1');
 jogador2.textContent = verificaPlacar('jogador2');
 
 campos.forEach(campo => {
     campo.addEventListener('click', () => {
-        if (!(jogada % 2)) {
+        const vezJogador1 = verificaVez();
+        if(vezJogador1) {
             campo.textContent = 'X';
             camposX.push(parseInt(campo.getAttribute("value")));
-            if (verificaVitoria(camposX)) {
+            if(verificaVitoria(camposX)) {
                 aumentaPlacar('jogador1');
                 jogador1.textContent = verificaPlacar('jogador1');
                 reiniciaOJogo();
             }else if(jogada == 8) {
-                info.textContent = 'Deu velha!';
-                info.classList.add('empate');
+                deuVelha();
                 reiniciaOJogo();
-            };
+            }else{
+                trocaVezDeJogar();
+            }
         }
         else {
             campo.textContent = 'O';
@@ -42,9 +50,13 @@ campos.forEach(campo => {
                 aumentaPlacar('jogador2');
                 jogador2.textContent = verificaPlacar('jogador2');
                 reiniciaOJogo();
+            }else if(jogada == 8) {
+                deuVelha();
+                reiniciaOJogo();
+            }else{
+                trocaVezDeJogar();
             }
         }
-        console.log(jogada);
         jogada++;
     })
 });
@@ -88,6 +100,35 @@ function verificaPlacar(placarDoJogador) {
 
 function aumentaPlacar(placarDoJogador) {
     sessionStorage.setItem(placarDoJogador, verificaPlacar(placarDoJogador) + 1);
+    trocaVezDeComecar(placarDoJogador);
+}
+
+function verificaVez() {
+    const vezObj = JSON.parse(sessionStorage.getItem('vez'));
+    if(vezObj.jogador1) return true;
+    return false;
+}
+
+function trocaVezDeJogar() {
+    vez.jogador1 = !vez.jogador1;
+    vez.jogador2 = !vez.jogador2;
+    sessionStorage.setItem('vez', JSON.stringify(vez));
+}
+
+function trocaVezDeComecar(vencedor) {
+    if(vencedor === 'jogador1'){
+        vez.jogador1 = true;
+        vez.jogador2 = false;
+    }else {
+        vez.jogador1 = false;
+        vez.jogador2 = true;
+    }
+    sessionStorage.setItem('vez', JSON.stringify(vez));
+}
+
+function deuVelha(){
+    info.textContent = 'Deu velha!';
+    info.classList.add('empate');
 }
 
 function reiniciaOJogo() {
